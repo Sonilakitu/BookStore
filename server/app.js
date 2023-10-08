d39bc67ac7dd8e27cf85970f7241afc0e1f6d514
Sonila Mohanty <sonilamohanty26@gmail.com>
2023-10-08 13:06:36 +0530

    Login and new user API Support

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// In-memory user database
+const users = [];
+
+// Register a new user
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send('Username and password are required.');
+  }
+  const newUser = { username, password };
+  users.push(newUser);
+  console.log(users)
+  res.status(201).send('User registered successfully.');
+});
+
+// Login and authenticate a user
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username && u.password === password);
+  if (!user) {
+    return res.status(401).send('Invalid username or password.');
+  }
+  res.status(200).send('Login successful.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
